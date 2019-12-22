1013c3e39d70f2de9802ebf00e8aad39af152eee
Andre Pool <git@andrepool.com>
2019-12-22 18:51:03 +0100

    move images from r4 to date directory, add images from other robot's, now a total of 565 annotated images available, captured in different light conditions

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # robocup_ml
 robocup data for machine learning
+
+Copyright 2019 Andre Pool
+
+SPDX-License-Identifier: Apache-2.0
+
+
+
+| r1/20191203 | plaze | non-homogeneous light |
+| r2/20190606 | the Locht | perfect light | 
+| r3/20190707 | Sydney | limited light |
+| r4/20190606 | the Locht | perfect light |
+| r4/20190707 | Sydney | limited light |
+| r4/20190926 | plaza | limited/non-homogeneous light |
+| r6/20190926 | plaza | too dark |
+
+
--- split_dataset.py
+++ split_dataset.py
@@ -1,31 +1,45 @@
 #!/usr/bin/env python
 
+# Copyright 2019 Andre Pool
+# SPDX-License-Identifier: Apache-2.0
+
+# split all jpg's over the train.tx and test.txt
+
 import glob, os
 
-# Current directory
-# current_dir = os.path.dirname(os.path.abspath(__file__))
+image_dirs = []
+image_dirs.append('r1/20191203')
+image_dirs.append('r2/20190606')
+image_dirs.append('r3/20190707')
+image_dirs.append('r4/20190606')
+image_dirs.append('r4/20190707')
+image_dirs.append('r4/20190926')
+image_dirs.append('r6/20190926')
 
-# print(current_dir)
+train_set = open('train.txt', 'w')  
+test_set = open('test.txt', 'w')
+train_amount = 0
+test_amount = 0
 
-final_dir = 'robocup_ml/r4'
-search_dir = 'r4'
 
-# Percentage of images to be used for the test set
-percentage_test = 10;
+for image_dir in image_dirs:
+   print('collect from directory: %s' % image_dir )
+   
+   # re-start the split counter for each directory, so once a jpg is part of the
+   # train set, it will not become part of the test set if another directory is added
+   split_counter = 0
 
-# Create and/or truncate train.txt and test.txt
-file_train = open('train.txt', 'w')  
-file_test = open('test.txt', 'w')
+   for file_name in glob.glob(image_dir +"/*.jpg") :
+      # print("%s" % file_name )
+      if split_counter == 9 : # 10 percent excluded for testing
+         test_set.write("robocup_ml/" + file_name + "\n")
+         split_counter = 0
+         test_amount += 1
+      else :
+         train_set.write("robocup_ml/" + file_name + "\n")
+         split_counter += 1
+         train_amount += 1
+      
 
-# Populate train.txt and test.txt
-counter = 1  
-index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.iglob(os.path.join(search_dir, "*.jpg")):  
-   title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-
-   if counter == index_test:
-      counter = 1
-      file_test.write(final_dir + "/" + title + '.jpg' + "\n")
-   else:
-      file_train.write(final_dir + "/" + title + '.jpg' + "\n")
-      counter = counter + 1
+print('train jpg: %d, test jpg: %d and total jpg: %d' % (train_amount, test_amount, train_amount + test_amount))
+print('all done')
